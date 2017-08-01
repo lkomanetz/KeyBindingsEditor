42c0f7ab3e79350fc327b49c96241d28e5f7e5d6
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-08-01 16:14:52 -0500

    Bringing asynchronous methods to process manager.

--- KeyPad/ProcessWatcher/ViewModels/ProcessWatcherViewModel.cs
+++ KeyPad/ProcessWatcher/ViewModels/ProcessWatcherViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -12,15 +13,17 @@ namespace KeyPad.ProcessWatcher.ViewModels {
 
 	internal class ProcessWatcherViewModel : INotifyPropertyChanged {
 
-		private const double TIMER_INTERVAL = 500D;
+		private const double TIMER_INTERVAL = 250D;
 		private ICommand _stopProcessCommand;
 		private ICommand _startProcessCommand;
 		private System.Timers.Timer _executeThreadTimer;
 		private Thread _watcherThread;
 		private IProcessManager _processManager;
 		private bool _isProcessRunning;
+		private bool _buttonEnabled;
 
 		public ProcessWatcherViewModel(IProcessManager processManager) {
+			_buttonEnabled = true;
 			_processManager = processManager;
 			_processManager.ProcessStarted += (sender, args) => InvokePropertyChangeEvents();
 			_processManager.ProcessStopped += (sender, args) => InvokePropertyChangeEvents();
@@ -37,14 +40,26 @@ namespace KeyPad.ProcessWatcher.ViewModels {
 				_watcherThread.Start();
 			};
 
-			_startProcessCommand = new DelegateCommand<object>((param) => _processManager.Start());
-			_stopProcessCommand = new DelegateCommand<object>((param) => _processManager.Stop());
+			_startProcessCommand = new DelegateCommand<object>((param) => ToggleActionAsync(() => _processManager.Start()));
+			_stopProcessCommand = new DelegateCommand<object>((param) => ToggleActionAsync(() => _processManager.Stop()));
 		} 
 
 		public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
 		public string LabelContent => (_isProcessRunning) ? "Running..." : "Stopped...";
 		public string ButtonLabelContent => (_isProcessRunning) ? "Stop" : "Start";
+
+		public bool ButtonEnabled {
+			get => _buttonEnabled;
+			private set {
+				if (_buttonEnabled == value)
+					return;
+
+				_buttonEnabled = value;
+				InvokePropertyChangeEvents();
+			}
+		}
+
 		public ICommand ButtonCommand => (_isProcessRunning) ? _stopProcessCommand : _startProcessCommand;
 		public Brush StatusColor => (_isProcessRunning) ? Brushes.Green : Brushes.Red;
 
@@ -53,11 +68,18 @@ namespace KeyPad.ProcessWatcher.ViewModels {
 			InvokePropertyChangeEvents();
 		}
 
+		private async void ToggleActionAsync(Action action) {
+			this.ButtonEnabled = false;
+			await Task.Factory.StartNew(() => action?.Invoke());
+			this.ButtonEnabled = true;
+		}
+
 		private void InvokePropertyChangeEvents() {
-			PropertyChanged(this, new PropertyChangedEventArgs("StatusColor"));
 			PropertyChanged(this, new PropertyChangedEventArgs("ButtonLabelContent"));
 			PropertyChanged(this, new PropertyChangedEventArgs("ButtonCommand"));
+			PropertyChanged(this, new PropertyChangedEventArgs("ButtonEnabled"));
 			PropertyChanged(this, new PropertyChangedEventArgs("LabelContent"));
+			PropertyChanged(this, new PropertyChangedEventArgs("StatusColor"));
 		}
 
 	}
