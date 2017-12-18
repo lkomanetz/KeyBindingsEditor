4e733510b7ef2b98ed22f72ff8afa557b4c67baa
Logan K <logan.komanetz@gmail.com>
2017-12-17 18:59:47 -0600

    I forgot to add a couple files.

---
+++ KeyPad/ViewModels/IFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPad.ViewModels {
+
+	public interface IFormViewModel : IViewModel {
+
+		string Title { get; }
+		bool IsDirty { get; }
+
+	}
+
+}
